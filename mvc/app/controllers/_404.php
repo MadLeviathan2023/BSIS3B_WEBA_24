f5490fc5cefcf4745daed527f425cd25416b0b11
equinox1123 <carllesternunez0410@gmail.com>
2024-03-01 08:25:00 +0800

    added 404 code

--- mvc/app/controllers/_404.php
+++ mvc/app/controllers/_404.php
@@ -1,6 +1,6 @@
 <?php
     class _404 extends controller{
-        public function index($param){
-            $this->view($param);
+        public function index(){
+            $this->view('404');
         }
     }
