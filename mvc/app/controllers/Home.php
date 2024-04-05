156be55ae858696b072fd16290ec057dc32858eb
Leonard Paulo Sanchez <leonardpaulo.sanchez.3@gmail.com>
2024-04-05 09:38:25 +0800

    added models

--- mvc/app/controllers/Home.php
+++ mvc/app/controllers/Home.php
@@ -1,8 +1,6 @@
 <?php
     class Home extends Controller{
         public function index(){
-            $user = new User();
-            $result = $user->delete('2', 'user_id');
             $this->view('home');
         }
     }
