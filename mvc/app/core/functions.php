7ea78924228e488359a75ad873122579fe4f56c6
Leonard Paulo Sanchez <leonardpaulo.sanchez.3@gmail.com>
2024-04-07 18:32:16 +0800

    removed datenow function

--- mvc/app/core/functions.php
+++ mvc/app/core/functions.php
@@ -4,9 +4,3 @@
         print_r($stuff);
         echo '</pre>';
     }
-
-    function datenow(){
-        $datetime = new DateTime();
-        $datetime->setTimeZone(new DateTimeZone('Asia/Manila'));
-        return $datetime;
-    }
