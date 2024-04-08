492aeb8b2d45b53fa6f95326c144c45d2d1cfbb5
Leonard Paulo Sanchez <leonardpaulo.sanchez.3@gmail.com>
2024-04-08 19:12:12 +0800

    updated js TypingAnim class

--- mvc/public/js/TypingAnim.js
+++ mvc/public/js/TypingAnim.js
@@ -1,8 +1,9 @@
 export class TypingAnim {
-    constructor(msgArr, elem, loop = true) {
+    constructor(msgArr, elem, loop = true, intervalDelay = 100) {
         this.msgArr = msgArr;
         this.elem = elem;
-        this.intervalDelay = 100;
+        this.defaultInterval = intervalDelay;
+        this.intervalDelay = intervalDelay;
         this.i = 0;
         this.j = 0;
         this.k = 0;
@@ -20,20 +21,20 @@ export class TypingAnim {
                     this.j++;
                 } else if (this.j === this.msgArr[this.i].length) {
                     if (this.k === 0) {
-                        clearInterval(this.timer);
+                        this.stop();
                         if (this.i < this.msgArr.length && this.loop == true){
                             setTimeout(() => {
-                                    this.timer = setInterval(this._animateMsg.bind(this), this.intervalDelay);
+                                    this.start();
                             }, 750);
                         }
                     } else if (this.k > 0) {
-                        if (this.intervalDelay === 100) {
+                        if (this.intervalDelay === this.defaultInterval) {
                             this._changeIntervalDelay(50);
                         }
                         const sliced = this.msgArr[this.i].slice(0, -this.k);
                         this.elem.innerHTML = sliced;
                         if (this.k === this.j) {
-                            this._changeIntervalDelay(100);
+                            this._changeIntervalDelay(this.intervalDelay);
                             this.j = 0;
                             this.k = -1;
                             this.i++;
@@ -47,14 +48,14 @@ export class TypingAnim {
             }
         } catch (err) {
             console.log(err);
-            clearInterval(this.timer);
+            this.stop();
         }
     }
 
     _changeIntervalDelay(newDelay) {
         this.intervalDelay = newDelay;
-        clearInterval(this.timer);
-        this.timer = setInterval(this._animateMsg.bind(this), this.intervalDelay);
+        this.stop();
+        this.start();
     }
 
     start(){
