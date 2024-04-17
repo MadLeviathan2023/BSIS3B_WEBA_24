97ccfb03ca3ad2c75c52cdc1083c8892139172ea
Leonard Paulo Sanchez <leonardpaulo.sanchez.3@gmail.com>
2024-04-17 22:40:02 +0800

    updated WebScanner

--- mvc/app/templates/web-scanner.php
+++ mvc/app/templates/web-scanner.php
@@ -1,16 +1,20 @@
 <dialog id="qr-scanner-dialog">
-    <div class="qr-scanner-layout">    
+    <div class="qr-scanner-layout"> 
+        <button id="btn-close-scanner">
+            <i class="fa-solid fa-circle-xmark"></i>
+        </button>
         <video id="qr-scanner"></video>
         <div class="camera-design-1">
             <div class="camera-design-2">
                 <div class="camera-design-3"></div>
             </div>
         </div>
-        <div class="qr-scanner-buttons">
-            <button id="btn-close-scanner">
-                <i class="fa-solid fa-circle-xmark"></i>
-                Close
-            </button>
+        <div class="qr-scanner-footer">
+            <div class="camera-list-container">
+                <i class="fa-solid fa-camera-rotate"></i>
+                <select id="selCamList">
+                </select>
+            </div>
         </div>
     </div>
 </dialog>
--- mvc/public/js/WebScanner.js
+++ mvc/public/js/WebScanner.js
@@ -4,10 +4,15 @@ import QrScanner from './qr-scanner/node_modules/qr-scanner/qr-scanner.min.js';
 const QR_SCANNER_DIALOG = document.getElementById('qr-scanner-dialog');
 const QR_SCANNER_ELEM = document.getElementById('qr-scanner');
 const BTN_CLOSE_SCANNER = document.getElementById('btn-close-scanner');
+const SEL_CAM_LIST = document.getElementById('selCamList');
 var qrScanner;
 
 QR_SCANNER_DIALOG.onclose = () => {
     qrScanner.stop();
+}
+
+SEL_CAM_LIST.onchange = async (e) => {
+    await qrScanner.setCamera(e.target.value);
 }
 
 export class WebScanner{
@@ -24,10 +29,20 @@ export class WebScanner{
             navigator.mediaDevices.getUserMedia({
                 video: true
             }).then(() => {
+                SEL_CAM_LIST.innerHTML = '';
                 QrScanner.hasCamera().then((hasCamera) => {
                     if (hasCamera) {
                         QR_SCANNER_DIALOG.showModal();
-                        qrScanner.start();
+                        qrScanner.start().then(() => {
+                            QrScanner.listCameras(true).then((cameras) => {
+                                cameras.forEach((camera) => {
+                                    const OPTION_ELEM = document.createElement('option');
+                                    OPTION_ELEM.value = camera.id;
+                                    OPTION_ELEM.textContent = camera.label;
+                                    SEL_CAM_LIST.append(OPTION_ELEM);
+                                });
+                            });
+                        });
                     } else {
                         Modal.Show('No camera available!', 'Failed', ModalButton.OK);
                     }
