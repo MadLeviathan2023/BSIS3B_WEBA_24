5d561bf039480be1257924d815a27f9a5b8fd1d3
Leonard Paulo Sanchez <leonardpaulo.sanchez.3@gmail.com>
2024-04-19 11:47:04 +0800

    added list-of-tasks

---
+++ list-of-tasks/app/core/Component.php
@@ -0,0 +1,20 @@
+<?php
+
+    class Component{
+        public function __construct(){
+            if (!property_exists($this, 'name')){
+                $this->name = strtolower($this::class);
+            }
+        }
+
+        public function load(){
+            $template = '../app/templates/' . strtolower($this->name) . '.php';
+            if (file_exists($template)){
+                require $template;
+            }
+            else{
+                $template404 = '../app/templates/template404.php';
+                require $template404;
+            }
+        }
+    }
