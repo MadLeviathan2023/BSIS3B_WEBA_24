bdc6c722b9956a54a7704a39d02ca8a55340a226
Leonard Paulo Sanchez <leonardpaulo.sanchez.3@gmail.com>
2024-04-19 12:58:48 +0800

    some update

--- eMenu/app/core/config.php
+++ eMenu/app/core/config.php
@@ -1,7 +1,7 @@
 <?php
     define('APP_NAME', 'eMenu');
     define('APP_LOGO', 'eMenu.png');
-    define('ROOT', 'https://'. $_SERVER['SERVER_NAME'] .'/Web/WebA-MVC/mvc/public');
+    define('ROOT', 'https://'. $_SERVER['SERVER_NAME'] .'/Web/WebA-MVC/eMenu/public');
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'e_menu');
     define('DB_USER', 'root');
