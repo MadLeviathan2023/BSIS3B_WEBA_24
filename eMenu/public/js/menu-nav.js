21a97f9bfe9194fee9c07a3f85221b82c5c12b0c
Leonard Paulo Sanchez <leonardpaulo.sanchez.3@gmail.com>
2024-04-27 14:41:30 +0800

    updated Menu Nav

--- eMenu/public/js/menu-nav.js
+++ eMenu/public/js/menu-nav.js
@@ -1,22 +1,32 @@
-var isVisible = false;
-
+const MOBILE_VW = 767;
 const OPTIONS = document.querySelector('nav ul.options');
 const BTN_MENU = document.getElementById('btnMenu');
-
-function toggleOptionsDisplay(){
-    if (!isVisible){
-        OPTIONS.style.display = 'block';
-    }
-    else{
-        OPTIONS.style.display = 'none';
-    }
-}
+var vw = window.visualViewport.width;
 
 BTN_MENU.onclick = () => {
     BTN_MENU.classList.toggle('active');
+    toggleDisplay(OPTIONS);
 }
 
-var vw = window.visualViewport.width;
+if (vw < MOBILE_VW){
+    toggleDisplay(OPTIONS);
+}
+
 window.addEventListener('resize', () => {
-    
+    vw = window.visualViewport.width;
+    if (vw < MOBILE_VW){
+        toggleDisplay(OPTIONS);
+    }
+    else{
+        OPTIONS.style.display = 'flex';
+    }
 });
+
+function toggleDisplay(elem){
+    if (elem.style.display == 'none'){
+        elem.style.display = vw < 767 ? 'block' : 'flex';
+    }
+    else{
+        elem.style.display = 'none';
+    }
+}
